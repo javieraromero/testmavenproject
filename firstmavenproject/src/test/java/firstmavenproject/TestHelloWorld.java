4a3d42d0b859db64026e74294328e107c3fb1885
Javier Romero <javier_aromero@Hotmail.com>
2020-09-17 17:21:02 -0400

    init commit

---
+++ firstmavenproject/src/test/java/firstmavenproject/TestHelloWorld.java
@@ -0,0 +1,12 @@
+package firstmavenproject;
+
+import org.junit.jupiter.api.Test;
+
+public class TestHelloWorld {
+
+	@Test
+	public void testHelloWorld()
+	{
+		System.out.println("Hello world");
+	}
+}
